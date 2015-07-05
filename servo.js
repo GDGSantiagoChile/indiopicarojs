743eae1eeaa274a2addc05cf0776cad095e22fdf
fforres mac <felipe.torressepulveda@gmail.com>
2015-07-05 19:18:25 -0300

    beta testing

--- servo.js
+++ servo.js
@@ -5,12 +5,9 @@ var controller = process.argv[2] || "GP2Y0A02YK0F";
 board.on("ready", function() {
     var servo = new five.Servo({
         pin: 10,
-        range: [0, 180],
+        range: [0, 360],
         startAt: 0,
         type: 'continuous'
     });
-
-    servo.to(0);
     servo.to(180);
-
 });
--- test.js
+++ test.js
@@ -1,32 +1,40 @@
-var five = require("johnny-five");
+'use strict';
+var five = require('johnny-five');
 var board = new five.Board();
 var Twitter = require('node-tweet-stream');
 var t = new Twitter({
-    consumer_key: '8Q9T8yWUaPHnqgRTAnS1maR5K',
-    consumer_secret: 'XcThyabsygUQPfJHpOQfFJttyjOVg3zITEwj4pRROExzcCU8K1',
-    token: '44652089-f3Y09zedoTIRilolC6tIJYrQZ5yp2yS17ijxUwRzk',
-    token_secret: '85tIzY4XSYn4n5lrkokWpHzm5PnjIUW0y7EbEM0oHh3nk'
+   consumer_key: '8Q9T8yWUaPHnqgRTAnS1maR5K',
+   consumer_secret: 'XcThyabsygUQPfJHpOQfFJttyjOVg3zITEwj4pRROExzcCU8K1',
+   token: '44652089-f3Y09zedoTIRilolC6tIJYrQZ5yp2yS17ijxUwRzk',
+   token_secret: '85tIzY4XSYn4n5lrkokWpHzm5PnjIUW0y7EbEM0oHh3nk'
 });
 
 
-board.on("ready", function() {
-    var led = new five.Led(13);
-    t.on('tweet', function(tweet) {
-        if (tweet.text.toLowerCase().indexOf("levantate") > -1) {
-            led.blink(500);
-            //SUBIR INDIO PICARO
-        } else if (tweet.text.toLowerCase().indexOf("bajate") > -1) {
-            //BAJAR INDIO PICARO
-            led.off();
-        }
-    });
-    t.on('error', function(err) {
-        console.log('Oh no');
-    });
-    t.track('@indiopicaro_js');
+var subirIndio = function(servo) {
+   servo.to(0);
+};
+
+var bajarIndio = function(servo) {
+   servo.to(180);
+};
+
+board.on('ready', function() {
+   var servo = new five.Servo({
+      pin: 10,
+      startAt: 0
+   });
+   t.on('tweet', function(tweet) {
+      if (tweet.text.toLowerCase().indexOf('levantate') > -1) {
+         //SUBIR INDIO PICARO
+         subirIndio(servo);
+      } else if (tweet.text.toLowerCase().indexOf('bajate') > -1) {
+         //BAJAR INDIO PICARO
+         bajarIndio(servo);
+      }
+   });
+   t.on('error', function(err) {
+      console.log('Oh no');
+   });
+   t.track('@indiopicaro_js');
 });
 
-
-/*
-
-*/
